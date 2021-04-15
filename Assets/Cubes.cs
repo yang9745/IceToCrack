e53ccf399d9bc8953703b6a82f6df2790771f9ca
yang9745 <1031418131@qq.com>
2021-04-15 13:41:40 +0800

    冰的素材
    已经导入

---
+++ Assets/Cubes.cs
@@ -0,0 +1,36 @@
+﻿/*************************************
+*
+*版本：     V1.0.0
+*创建人： 杨晓涛
+*日期：    21/X/X
+*作用：    未知
+*描述：    未知
+*
+***********************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubes : MonoBehaviour
+{
+    public bool b = false;
+    void Start()
+    {
+        
+    }
+    float _time=0;
+    // Update is called once per frame
+    void Update()
+    {
+        if(b==true)
+        {
+            _time += Time.deltaTime;
+            if(_time>=1)
+            {
+                _time = 0;
+                b = false;
+            }
+            transform.position += new Vector3(0, -10, 0)*Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/T.cs
@@ -0,0 +1,32 @@
+﻿/*************************************
+*
+*版本：     V1.0.0
+*创建人： 杨晓涛
+*日期：    21/X/X
+*作用：    未知
+*描述：    未知
+*
+***********************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T : MonoBehaviour
+{
+    public Camera mainCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit,200,LayerMask.GetMask("Water")))
+            {
+                hit.transform.GetComponent<Cubes>().b = true; 
+            }
+        }
+    }
+
+}
